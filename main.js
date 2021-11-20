4703ad30dcd0c3b902cbef1b662284151b5d7900
aanya2707 <85337689+aanya2707@users.noreply.github.com>
2021-11-20 17:41:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function addUser(){
+    player1= document.getElementById("user_name_1").value ;
+    player2= document.getElementById("user_name_2").value ; 
+
+    localStorage.setItem("Player_1" , player1);
+    localStorage.setItem("Player_2" , player2);
+
+    window.location="quiz_game_page.html";
+}
